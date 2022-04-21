8fd5da1ff09a83be2e687b13c7bd28545d991fb9
MkQtS <shuoyibuer.63@gmail.com>
2022-04-21 19:24:31 +0800

    Update rules and add script

---
+++ CNKI-Redirect.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         重定向知网至海外版
+// @namespace    cnki_redirect_to_oversea
+// @description  将部分知网界面重定向到海外版知网
+// @version      1.0
+// @icon         https://cnki.net/favicon.ico
+// @author       MkQtS
+// @homepage     https://github.com/MkQtS/CNKI-Redirect
+// @match        *://cnki.net/*
+// @match        *://*.cnki.net/*
+// @match        *://*.cnki.com.cn/*
+// @exclude      *://chn.oversea.cnki.net/kcms/detail/*
+// @grant        none
+// ==/UserScript==
+
+/**
+Chrome等浏览器支持添加自定义搜索引擎，CNKI海外版格式：
+https://chn.oversea.cnki.net/kns/defaultresult/index?&kw=%s
+**/
+
+(function () {
+
+    //知网空间-cdmd
+    if (window.location.href.match(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w{4})-\d+-(\d+)\.htm.*$/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w{4})-\d+-(\d+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2.nh');
+    };
+
+    //知网空间-cpfd
+    if (window.location.href.match(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/\w{4}total-\w+\.htm/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w{4})total-(\w+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+    //知网空间-www
+    if (window.location.href.match(/^https?:\/\/www\.cnki\.com\.cn\/article\/\w{4}total-\w+\.htm/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w{4})total-(\w+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+    //知网-非ste
+    if (window.location.href.match(/^https?:\/\/(?!ste\.cnki)\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=(\w+))(?=.*filename\=([\w\.]+))/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/(?!ste\.cnki)\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+    //知网-非ste-无dbcode
+    if (window.location.href.match(/^https?:\/\/(?!ste\.cnki)\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=(\w+))(?=.*filename\=([\w\.]+))/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/(?!ste\.cnki)\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+    //知网-ste
+    if (window.location.href.match(/^https?:\/\/ste\.cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=(\w+))(?=.*filename\=([\w\.]+))/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/ste\.cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+    //知网-ste-无dbcode
+    if (window.location.href.match(/^https?:\/\/ste\.cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=(\w+))(?=.*filename\=([\w\.]+))/i)) {
+        window.location.href = window.location.href.replace(/^https?:\/\/ste\.cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
+    };
+
+})();
--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # CNKI-Redirect
 
-Header Editor规则：将知网部分界面重定向到海外版知网
+作用：将知网部分界面重定向到海外版知网
 
-## Step 1: 安装Header Editor
+## 方法1：油猴脚本
 
-|[Chrome(Chromium)](https://chrome.google.com/webstore/detail/header-editor/eningockdidmgiojffjmkdblpjocbhgh)|[Firefox](https://addons.mozilla.org/zh-CN/firefox/addon/header-editor/)|[GitHub](https://github.com/FirefoxBar/HeaderEditor)|
+在浏览器中安装油猴插件后安装[此脚本](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.js)
+
+## 方法2：Header Editor
+
+首先安装[Header Editor](https://github.com/FirefoxBar/HeaderEditor)插件：
+
+|[Chrome Store](https://chrome.google.com/webstore/detail/header-editor/eningockdidmgiojffjmkdblpjocbhgh)|[Firefox Store](https://addons.mozilla.org/zh-CN/firefox/addon/header-editor/)|[GitHub](https://github.com/FirefoxBar/HeaderEditor)|
 |--|--|--|
 
-## Step 2: 导入规则
-
-在Header Editor中导入[bib文件](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.bib)
+然后在Header Editor中导入此[规则文件](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.bib)
 
