14d4a692ab58c16165216428be8e3c4d8e77b008
MkQtS <81752398+MkQtS@users.noreply.github.com>
2022-04-24 14:57:50 +0800

    Update CNKI-Redirect.user.js

--- CNKI-Redirect.user.js
+++ CNKI-Redirect.user.js
@@ -1,74 +1,80 @@
 // ==UserScript==
 // @name         重定向知网至海外版
 // @namespace    cnki_redirect_to_oversea
-// @description  将部分知网界面重定向到海外版知网
-// @version      1.1
+// @description  将部分知网界面重定向到海外版知网，支持多数知网文献页、知网空间和手机知网。
+// @version      1.2
 // @icon         https://cnki.net/favicon.ico
 // @author       MkQtS
 // @homepage     https://github.com/MkQtS/CNKI-Redirect
 // @match        *://cnki.net/*
 // @match        *://*.cnki.net/*
 // @match        *://*.cnki.com.cn/*
-// @exclude      *://chn.oversea.cnki.net/*
-// @exclude      *://web01.cnki.net/*
 // @grant        none
 // ==/UserScript==
 
-/**
-Chrome等浏览器支持添加自定义搜索引擎，CNKI海外版格式：
-https://chn.oversea.cnki.net/kns/defaultresult/index?&kw=%s
-**/
+(function () {
 
-(function() {
-    var current_url=window.location.href;
+    var current_url = window.location.href;
+    var situation, dbcode, filename;
 
-    //知网空间 CNKI.COM.CN
-    if (current_url.match(/^https?:\/\/\S+\.cnki\.com\.cn\/article\//i)) {
-        //cdmd
-        if (current_url.match(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/\w{4}-\d+-\d+\.htm.*$/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w{4})-\d+-(\d+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2.nh');
-        }
-        //cpfd
-        else if (current_url.match(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/\w{4}total-\w+\.htm/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w{4})total-(\w+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
-        }
-        //cyfd
-        else if (current_url.match(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/\w+\.htm/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/(\w+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=CYFD&filename=$1');
-        }
-        //www
-        else if (current_url.match(/^https?:\/\/www\.cnki\.com\.cn\/article\/\w{4}total-\w+\.htm/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w{4})total-(\w+)\.htm.*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
-        }
-        else {}
+    if (current_url.match(/^https?:\/\/chn\.oversea\.cnki\.net\/kcms\/detail\/detail\.aspx\?dbcode=[^&]+&filename=[^&]+$/i)) {
+        situation = 'skip';//IDEAL URL
+    }
+    else if (current_url.match(/^https?:\/\/\S+\.cnki\.com\.cn\/article\//i)) {
+        situation = 'space';//CNKI.COM.CN
+    }
+    else if (current_url.match(/^https?:\/\/wap\.cnki\.net\/touch\/web\//i)) {
+        situation = 'wap';//WAP.CNKI.NET
+    }
+    else if (current_url.match(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail/i)) {
+        situation = 'common';//OTHER CNKI.NET
+    }
+    else {
+        situation = 'skip';//NOT MATCH
     };
 
-    //知网 CNKI.NET
-    if (current_url.match(/^https?:\/\/\S*\.?cnki\.net\//i)) {
-        //wap
-        if (current_url.match(/^https?:\/\/wap\.cnki\.net\/touch\/web\//i)) {
-            var wap_article = document.getElementById('a_download').onclick.toString();
-            var wap_article_info=wap_article.replace(/^[\S\s]+GetDownloadInfo_\d+\('(\w+)','(\w+)'\)[\S\s]+/,'dbcode=$2&filename=$1');
-            var wap_oversea_link = 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?' + wap_article_info;
-            window.location.href = wap_oversea_link;
-        }
-        //common-dbcode
-        else if (current_url.match(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=\w+)(?=.*filename\=[\w\.]+)/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?=.*dbcode\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=$1&filename=$2');
-        }
-        //common-dbname
-        else if (current_url.match(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=\w+)(?=.*filename\=[\w\.]+)/i)) {
-            window.location.href = current_url.replace(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail\/detail\.aspx\?(?!.*dbcode\=\w+)(?=.*dbname\=(\w+))(?=.*filename\=([\w\.]+)).*$/i, 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbname=$1&filename=$2');
-        }
-        //other situations
-        else if (current_url.match(/^https?:\/\/\S*\.?cnki\.net\/kcms\/detail/i)) {
-            var var_dbcode = document.getElementById('paramdbcode').value;
-            var var_filename = document.getElementById('paramfilename').value;
-            var oversea_link = 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=' + var_dbcode + '&filename=' + var_filename;
-            window.location.href = oversea_link;
-        }
-        else{}
+    switch (situation) {
+        case 'skip':
+            break;
+        case 'space':
+            var spacetype = current_url.replace(/^https?:\/\/(\S+)\.cnki\.com\.cn\/.*$/i, '$1');
+            switch (spacetype) {
+                case 'cdmd':
+                    dbcode = current_url.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w{4})-\d+-\d+\.htm.*$/i, '$1');
+                    filename = current_url.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/\w{4}-\d+-(\d+)\.htm.*$/i, '$1.nh');
+                    break;
+                case 'cpfd':
+                    dbcode = current_url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w{4})total-\w+\.htm.*$/i, '$1');
+                    filename = current_url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/\w{4}total-(\w+)\.htm.*$/i, '$1');
+                    break;
+                case 'cyfd':
+                    dbcode = 'CYFD';
+                    filename = current_url.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/(\w+)\.htm.*$/i, '$1');
+                    break;
+                case 'www':
+                    dbcode = current_url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w{4})total-\w+\.htm.*$/i, '$1');
+                    filename = current_url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/\w{4}total-(\w+)\.htm.*$/i, '$1');
+                    break;
+                default:
+                    break;
+            };
+            break;
+        case 'wap':
+            var wap_article_info = document.getElementById('a_download').onclick.toString();
+            dbcode = wap_article_info.replace(/^[\S\s]+GetDownloadInfo_\d+\('\w+','(\w+)'\)[\S\s]+/, '$1');
+            filename = wap_article_info.replace(/^[\S\s]+GetDownloadInfo_\d+\('(\w+)','\w+'\)[\S\s]+/, '$1');
+            break;
+        case 'common':
+            dbcode = document.getElementById('paramdbcode').value;
+            filename = document.getElementById('paramfilename').value;
+            break;
     };
 
-})();
+    var new_url = 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?dbcode=' + dbcode + '&filename=' + filename;
+    if (dbcode && current_url != new_url) {
+        window.location.href = new_url;
+    };
+
+}
+)();
 
