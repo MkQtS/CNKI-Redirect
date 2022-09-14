2f4b0e4619f3360807ddf35e5c569224065b4958
MkQtS <81752398+MkQtS@users.noreply.github.com>
2022-09-14 18:18:38 +0800

    Use uppercase filename

--- CNKI-Redirect.user.js
+++ CNKI-Redirect.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         重定向知网至海外版
-// @namespace    cnki_redirect_to_oversea
+// @namespace    cnki_to_oversea
 // @description  将部分知网界面重定向到海外版知网，支持多数知网文献页、知网空间和手机知网。
-// @version      1.7
-// @icon         https://cnki.net/favicon.ico
+// @version      1.8
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAB10lEQVQ4jZVSP8hpcRj+nZs6oiPkpFBkoCxnQCeT0rdQyiCZDAwYTOQsBsOR/aRshlMymsSK/JkMysDgz3KOZDmO8t/vDu79fO51b33P9r71vO/7PO+DwKIbfAuw6Ia/IQhCPB6nKEoURfgWRbfkK/l0OrXb7dlsZrPZIpHI2wU/vha322273fp8Pq/X+6+LXgij0Wi32+VyueVymc1m1+v1/zRsNhuCIFQqVSwWUyqVarV6Op1+Hs9xnCiKsOh+Emia/pyi0+mazSaE8HK59Pv9TCbjcDjy+fyL6FAohGEYwzD3+73RaJjN5mq1utlsptOpxWK5Xq/dbhd8vNq63+8ZhjEajbVabbFYGAyGRCJxPB5Pp1MwGOx0Os8Nk8mE4zipVDoYDFarVblcdjqdBEFgGIaiaKVSQRDE5XKBHpAAAHie93g85/PZbrfP53OKotLptEajsVqtrVZLJpPRNO33+w+HAwaABAAAISRJEsfxbDZbr9f1er1Go0EQJJVKDYfDXq9XKpU8Ho9cLn/aKgjCw9lAIFAoFMLh8Gw2e3T2+/2baCgUCp7no9EoSZLRaNRkMkEIWZbFcfyPv/0ijMfjh6HJZFKr1bIsi6KoRCIBfwH5brx/AseDLUJKQoGcAAAAAElFTkSuQmCC
 // @author       MkQtS
 // @homepage     https://github.com/MkQtS/CNKI-Redirect
 // @updateURL    https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.user.js
@@ -20,45 +20,56 @@ var target = 'https://chn.oversea.cnki.net/kcms/detail/detail.aspx?';
 //var target = 'https://oversea.cnki.net/kcms/detail/detail.aspx?';
 //var target = 'https://tra.oversea.cnki.net/kcms/detail/detail.aspx?';
 
-var current_url = window.location.href;
-var situation, fileID;
+var currentUrl = window.location.href;
+var situation = 'skip';
+var fileID;
 
-if (current_url.match(/^https?:\/\/(\w+\.)?oversea\.cnki\.net\/kcms\/detail\/detail\.aspx\?dbcode=[\w]+&filename=[\w\.]+$/i)) {
-    situation = 'skip';
-}//IDEAL URL
-else if (current_url.match(/^https?:\/\/[^/]+\.cnki\.com\.cn\/article\//i)) {
-    situation = 'space';
-}//CNKI.COM.CN
-else if (current_url.match(/^https?:\/\/wap\.cnki\.net\/touch\/web\//i)) {
-    situation = 'wap';
-}//WAP.CNKI.NET
-else if (current_url.match(/^https?:\/\/([\w\.]+)?cnki\.net\/kcms\/detail/i)) {
-    situation = 'common';
-}//OTHER CNKI.NET
-else {
-    situation = 'skip';
-};//NOT MATCH
+const CNKITYPE = {
+    'IDEAL': {
+        Check: /^https?:\/\/(\w+\.)?oversea\.cnki\.net\/kcms\/detail\/detail\.aspx\?dbcode=[\w]+&filename=[\w\.]+$/i,
+        Type: 'skip'
+    },
+    'WAP.CNKI.NET': {
+        Check: /^https?:\/\/wap\.cnki\.net\/touch\/web\//i,
+        Type: 'wap'
+    },
+    'CNKI.COM.CN': {
+        Check: /^https?:\/\/[^/]+\.cnki\.com\.cn\/article\//i,
+        Type: 'space'
+    },
+    'COMMON': {
+        Check: /^https?:\/\/([\w\.]+)?cnki\.net\/kcms\/detail/i,
+        Type: 'common'
+    },
+};
+
+for (let i in CNKITYPE) {
+    if (CNKITYPE[i].Check.test(currentUrl)) {
+        situation = CNKITYPE[i].Type;
+        break;
+    };
+};
 
 switch (situation) {
     case 'skip':
         break;
     case 'space': {
-        let spacetype = current_url.replace(/^https?:\/\/(\S+)\.cnki\.com\.cn\/.*$/i, '$1');
+        let spacetype = currentUrl.replace(/^https?:\/\/([^/]+)\.cnki\.com\.cn\/.+$/i, '$1');
         switch (spacetype) {
             case 'cdmd':
-                fileID = current_url.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w+)-\d+-(\d+)\.htm.*$/i, 'dbcode=$1&filename=$2.nh');
+                fileID = currentUrl.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/(\w+)-\d+-(\d+)\.htm.*$/i, 'dbcode=$1&filename=$2.nh');
                 window.stop();
                 break;
             case 'cpfd':
-                fileID = current_url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w+)total-(\w+)\.htm.*$/i, 'dbcode=$1&filename=$2');
+                fileID = currentUrl.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w+)total-(\w+)\.htm.*$/i, 'dbcode=$1&filename=$2');
                 window.stop();
                 break;
             case 'cyfd':
-                fileID = current_url.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/(\w+)\.htm.*$/i, 'dbcode=CYFD&filename=$1');
+                fileID = currentUrl.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/(\w+)\.htm.*$/i, 'dbcode=CYFD&filename=$1');
                 window.stop();
                 break;
             case 'www':
-                fileID = current_url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w+)total-(\w+)\.htm.*$/i, 'dbcode=$1&filename=$2');
+                fileID = currentUrl.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w+)total-(\w+)\.htm.*$/i, 'dbcode=$1&filename=$2');
                 window.stop();
                 break;
             default:
@@ -67,9 +78,10 @@ switch (situation) {
         break;
     };
     case 'wap': {
-        let wap_article_info = document.getElementById('a_download').onclick.toString();
-        if (wap_article_info.match(/^[\S\s]+GetDownloadInfo_\d+\('[\w\.]+','\w+'[\S\s]+$/)) {
-            fileID = wap_article_info.replace(/^[\S\s]+GetDownloadInfo_\d+\('([\w\.]+)','(\w+)'[\S\s]+$/, 'dbcode=$2&filename=$1');
+        let wap_info_check = /^[\S\s]+GetDownloadInfo_\d+\('[\w\.]+','\w+'[\S\s]+$/;
+        let wap_info = document.getElementById('a_download').onclick.toString();
+        if (wap_info_check.test(wap_info)) {
+            fileID = wap_info.replace(/^[\S\s]+GetDownloadInfo_\d+\('([\w\.]+)','(\w+)'[\S\s]+$/, 'dbcode=$2&filename=$1');
         };
         break;
     };
@@ -77,6 +89,7 @@ switch (situation) {
         let dbcode = document.getElementById('paramdbcode').value;
         let filename = document.getElementById('paramfilename').value;
         if (dbcode && filename) {
+            filename = filename.toUpperCase().replace(/^([^.]+)\.NH$/i, '$1.nh');
             fileID = "dbcode=" + dbcode + "&filename=" + filename;
         };
         break;
@@ -84,7 +97,7 @@ switch (situation) {
 };
 
 if (fileID) {
-    let new_url = target + fileID;
-    window.location.href = new_url;
+    let newUrl = target + fileID;
+    window.location.href = newUrl;
 };
 
--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 # CNKI-Redirect
 
-作用：将知网文献页重定向到海外版知网
+## 作用
 
-## 方法1：油猴脚本
+将知网文献页重定向到海外版知网，支持多数知网文献页、知网空间和手机知网
 
-在浏览器中安装*Tampermonkey*插件后安装[此脚本](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.user.js)
+## 为什么要使用海外版知网？
 
-- 推荐，稳定但会慢一些
-- 支持多数知网文献页、知网空间和手机知网
+- 部分文献只在海外版提供PDF下载（海外版也能下载CAJ格式）
+- 手机知网（`wap.cnki.net`）不能通过IP免登陆下载文献
 
-## 方法2：Header Editor
+## 使用方法
 
-首先在浏览器中安装*Header Editor*插件：
+在浏览器中安装*Tampermonkey*类插件后安装[此脚本](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.user.js)
 
-|[Chrome Store](https://chrome.google.com/webstore/detail/header-editor/eningockdidmgiojffjmkdblpjocbhgh)|[Firefox Store](https://addons.mozilla.org/zh-CN/firefox/addon/header-editor/)|[GitHub](https://github.com/FirefoxBar/HeaderEditor)|
-|--|--|--|
-
-然后在Header Editor中导入此[规则文件](https://raw.githubusercontent.com/MkQtS/CNKI-Redirect/main/CNKI-Redirect.bib)
-
-- 纯URL匹配跳转，部分文献会出问题，但比较快
-- 支持部分知网文献页和知网空间
