2199b23bb76cd2870737729a563b3c937128cc5d
MkQtS <81752398+MkQtS@users.noreply.github.com>
2022-10-15 15:35:59 +0800

    filename with dot

--- CNKI-Redirect.user.js
+++ CNKI-Redirect.user.js
@@ -2,7 +2,7 @@
 // @name         重定向知网至海外版
 // @namespace    cnki_to_oversea
 // @description  将知网文献页重定向至海外版以便下载文献。支持知网空间、知网百科、知网阅读、知网文化及手机知网。
-// @version      2.5
+// @version      2.6
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAB10lEQVQ4jZVSP8hpcRj+nZs6oiPkpFBkoCxnQCeT0rdQyiCZDAwYTOQsBsOR/aRshlMymsSK/JkMysDgz3KOZDmO8t/vDu79fO51b33P9r71vO/7PO+DwKIbfAuw6Ia/IQhCPB6nKEoURfgWRbfkK/l0OrXb7dlsZrPZIpHI2wU/vha322273fp8Pq/X+6+LXgij0Wi32+VyueVymc1m1+v1/zRsNhuCIFQqVSwWUyqVarV6Op1+Hs9xnCiKsOh+Emia/pyi0+mazSaE8HK59Pv9TCbjcDjy+fyL6FAohGEYwzD3+73RaJjN5mq1utlsptOpxWK5Xq/dbhd8vNq63+8ZhjEajbVabbFYGAyGRCJxPB5Pp1MwGOx0Os8Nk8mE4zipVDoYDFarVblcdjqdBEFgGIaiaKVSQRDE5XKBHpAAAHie93g85/PZbrfP53OKotLptEajsVqtrVZLJpPRNO33+w+HAwaABAAAISRJEsfxbDZbr9f1er1Go0EQJJVKDYfDXq9XKpU8Ho9cLn/aKgjCw9lAIFAoFMLh8Gw2e3T2+/2baCgUCp7no9EoSZLRaNRkMkEIWZbFcfyPv/0ijMfjh6HJZFKr1bIsi6KoRCIBfwH5brx/AseDLUJKQoGcAAAAAElFTkSuQmCC
 // @author       MkQtS
 // @license      MIT
@@ -97,16 +97,16 @@
                         filename = url.replace(/^https?:\/\/cdmd\.cnki\.com\.cn\/article\/\w+-\d+-(\d+)\.htm.*$/i, '$1.nh');
                         break;
                     case 'cpfd':
-                        dbcode = url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w+)total-\w+\.htm.*$/i, '$1');
-                        filename = url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/\w+total-(\w+)\.htm.*$/i, '$1');
+                        dbcode = url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/(\w+)total-[\w\.]+\.htm.*$/i, '$1');
+                        filename = url.replace(/^https?:\/\/cpfd\.cnki\.com\.cn\/article\/\w+total-([\w\.]+)\.htm.*$/i, '$1');
                         break;
                     case 'cyfd':
                         dbcode = 'CYFD';
-                        filename = url.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/(\w+)\.htm.*$/i, '$1');
+                        filename = url.replace(/^https?:\/\/cyfd\.cnki\.com\.cn\/article\/([\w\.]+)\.htm.*$/i, '$1');
                         break;
                     case 'www':
-                        dbcode = url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w+)total-\w+\.htm.*$/i, '$1');
-                        filename = url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/\w+total-(\w+)\.htm.*$/i, '$1');
+                        dbcode = url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/(\w+)total-[\w\.]+\.htm.*$/i, '$1');
+                        filename = url.replace(/^https?:\/\/www\.cnki\.com\.cn\/article\/\w+total-([\w\.]+)\.htm.*$/i, '$1');
                         break;
                     default:
                         break;
@@ -120,7 +120,7 @@
             }
             case 'wenhua': {
                 dbcode = document.getElementById('journalimg').src.replace(/^https?:\/\/[^/]+\.cnki\.net\/([^/]+)\/.+$/i, '$1');
-                filename = url.replace(/^https?:\/\/wh\.cnki\.net\/article\/detail\/([^?]+).*$/i, '$1');
+                filename = url.replace(/^https?:\/\/wh\.cnki\.net\/article\/detail\/([\w\.]+).*$/i, '$1');
                 break;
             }
             case 'xuewen':
